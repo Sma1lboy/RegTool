1a2d06bf641d6fc48902bd04aa9e1433f14a8000
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-05-31 23:57:58 +0800

    to: remove some code to local sources

--- source/init.go
+++ source/init.go
@@ -101,32 +101,6 @@ func saveToBackup(source map[string]string) {
 	}
 }
 
-func readBackupFile() (map[string]string, error) {
-	var data map[string]string
-
-	// Check if the backup file exists
-	if _, err := os.Stat(SOURCE_BACKUP_FILE); err == nil {
-		// File exists, read the existing data
-		file, err := os.Open(SOURCE_BACKUP_FILE)
-		if err != nil {
-			return nil, fmt.Errorf("failed to open backup file: %v", err)
-		}
-		defer file.Close()
-
-		decoder := json.NewDecoder(file)
-		err = decoder.Decode(&data)
-		if err != nil {
-			return nil, fmt.Errorf("failed to decode backup file: %v", err)
-		}
-		return data, nil
-	} else if errors.Is(err, os.ErrNotExist) {
-		// File does not exist, return empty map
-		return make(map[string]string), nil
-	} else {
-		return nil, fmt.Errorf("failed to check backup file: %v", err)
-	}
-}
-
 func Run() map[string]string {
 	backup()
 	sources := map[string]string{
@@ -134,14 +108,4 @@ func Run() map[string]string {
 	}
 	saveToBackup(sources)
 	return sources
-}
-
-func ReadBackup() map[string]string {
-	m, err := readBackupFile()
-
-	if err != nil {
-		fmt.Println("Error reading backup file:", err)
-		return nil
-	}
-	return m
 }
