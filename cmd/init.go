926d8a4ee5500d670188e0cb99ff9d7b1ef336cc
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-05-31 19:09:38 +0800

    to: adding init command for init this project

--- cmd/init.go
+++ cmd/init.go
@@ -1,17 +1,21 @@
 package cmd
 
 import (
+	"fmt"
 	"registryhub/source"
 
 	"github.com/spf13/cobra"
 )
 
-
-var initCommand = &cobra.Command {
+var initCommand = &cobra.Command{
 	Use: "init",
 	Run: func(cmd *cobra.Command, args []string) {
-    source.Run()
-  },
+		sources := source.Run()
+		for v, s := range sources {
+			fmt.Println(v, s)
+		}
+
+	},
 }
 
 func init() {
