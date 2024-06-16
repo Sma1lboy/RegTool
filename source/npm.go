b81afe5d828281a1eadbe262daf0ab15a25b68cb
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-06-16 23:52:50 +0800

    to(npm): adding npm get and set registry method

--- source/npm.go
+++ source/npm.go
@@ -7,12 +7,24 @@ import (
 	"strings"
 )
 
-func getNpmRegistry() string {
+type NpmRegistryManager struct{}
+
+func (n NpmRegistryManager) GetCurrRegistry() (string, error) {
 	cmd := exec.Command("npm", "config", "get", "registry")
 	output, err := cmd.Output()
 	if err != nil {
 		fmt.Println("Error:", err)
-		return ""
+		return "", err
 	}
-	return strings.TrimSpace(string(output))
+	return strings.TrimSpace(string(output)), nil
+}
+
+func (n NpmRegistryManager) SetRegistry(region string) (string, error) {
+	rs, err := GetRemoteRegistrySources()
+	if err != nil {
+		return "", err
+	}
+	registry := (*rs)[StringToRegion(region)]["npm"][0]
+	//TODO real set up registry
+	return registry, nil
 }
